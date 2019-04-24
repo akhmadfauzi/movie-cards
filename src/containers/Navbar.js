b2ce7bef29ab46572c7e53be417cbe5b46e7448c
oji <sifauzi@gmail.com>
2019-04-24 16:53:56 +0700

    Reset navbar styles

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -11,28 +11,26 @@ class Navbar extends React.Component {
 
 	toggleClickHandler() {
 		this.props.toggleClickHandler();
-		const toggled = this.props.toggle;
-		const menu = document.querySelector('.menu-toggle+ul');
-		console.log(menu);
-		menu.className = 'toggle-open';
-		if(!toggled){
-			setTimeout(()=>{
-				menu.className = 'toggle-open slide-in';
-			},300);
-		}else{
-			menu.className = '';
-		}
+		// const toggled = this.props.toggle;
+		// const menu = document.querySelector('.menu-toggle+ul');
+		// console.log(menu);
+		// menu.className = 'toggle-open';
+		// if (!toggled) {
+		// 	setTimeout(() => {
+		// 		menu.className = 'toggle-open slide-in';
+		// 	}, 300);
+		// }
 	}
 
 	render() {
 		return (
-			<nav className="navbar navbar-fluid" >
+			<nav className="navbar">
 				<div className="navbar__content">
 					<div className="content__navbar-brand">MovieCards</div>
-					<div className="content__navbar-items">
-						<div className="menu-toggle" onClick={this.toggleClickHandler.bind(this)}>
-							<span><i className="fas fa-bars"></i></span>
-						</div>
+					<div className="menu-toggle" onClick={this.toggleClickHandler.bind(this)}>
+						<span><i className="fas fa-bars"></i></span>
+					</div>
+					<div className="collapse">
 						<ul>
 							<li><Link to="/">Home</Link> <span></span></li>
 							<li><Link to="/category/">Category</Link> <span></span></li>
@@ -46,8 +44,8 @@ class Navbar extends React.Component {
 	}
 
 }
-const mapStateToProps = (state)=>{
-	
+const mapStateToProps = (state) => {
+
 	return { toggle: state.ui.navbar.toggle }
 }
 export default connect(mapStateToProps, { toggleClickHandler })(Navbar);
