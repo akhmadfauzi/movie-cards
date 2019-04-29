9a3fc2baed9d04b022d257ca7cd7890afcc74bd2
oji <sifauzi@gmail.com>
2019-04-29 13:47:33 +0700

    Updated

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -12,13 +12,16 @@ class Navbar extends React.Component {
 	toggleClickHandler() {
 		this.props.toggleClickHandler();
 		const toggled = this.props.toggle;
+		console.log(toggled);
 		const menuWrapper = document.querySelector('.collapse');
-		const menu = document.querySelector('.collapse>ul');
+		
 		menuWrapper.className = 'collapse toggle-open';
 		if (!toggled) {
 			setTimeout(() => {
-				menu.className = 'collapse toggle-open slide-in';
-			}, 300);
+				menuWrapper.className = 'collapse toggle-open slide-in';
+			}, 500);
+		}else{
+			menuWrapper.className = 'collapse';
 		}
 	}
 
@@ -30,15 +33,16 @@ class Navbar extends React.Component {
 					<div className="menu-toggle" onClick={this.toggleClickHandler.bind(this)}>
 						<span><i className="fas fa-bars"></i></span>
 					</div>
+					<div className="collapse">
+						<ul>
+							<li><Link to="/">Home</Link> <span></span></li>
+							<li><Link to="/category/">Category</Link> <span></span></li>
+							<li><Link to="/about/">About</Link> <span></span></li>
+							<li><Link to="/contact/">Contact</Link> <span></span></li>
+						</ul>
+					</div>
 				</div>
-				<div className="collapse">
-					<ul>
-						<li><Link to="/">Home</Link> <span></span></li>
-						<li><Link to="/category/">Category</Link> <span></span></li>
-						<li><Link to="/about/">About</Link> <span></span></li>
-						<li><Link to="/contact/">Contact</Link> <span></span></li>
-					</ul>
-				</div>
+
 			</nav>
 		);
 	}
